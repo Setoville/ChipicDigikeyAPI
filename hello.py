6fa2cd802c6501b97311f83863c0af378de5671e
andrew <andrewseto@live.com>
2018-08-24 23:55:33 -0400

    Adding more filters

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import http.client
+import json
 import config
 app = Flask(__name__)
 conn = http.client.HTTPSConnection("api.digikey.com")
@@ -10,15 +11,17 @@ def hello_world():
 @app.route('/<model>',methods=['GET'])
 def get_things(model):
 
-	payload = "{\"SearchOptions\":[\"CollapsePackingTypes\"],\"Keywords\":\"p5555-nd\",\"RecordCount\":\"10\",\"RecordStartPosition\":\"0\",\"Filters\":{\"CategoryIds\":[57203494],\"FamilyIds\":[69149394],\"ManufacturerIds\":[44865313],\"ParametricFilters\":[{\"ParameterId\":\"725\",\"ValueId\":\"7\"}]},\"Sort\":{\"Option\":\"SortByUnitPrice\",\"Direction\":\"Ascending\",\"SortParameterId\":\"50\"},\"RequestedQuantity\":\"50\"}"
-	client_id = config.HT6_CLIENT_ID
-	access_token = config.HT6_ACCESS_TOKEN
+	payloadFirstHalf = "{\"SearchOptions\":[\"CollapsePackingTypes\"],\"Keywords\":\""
+	payloadSecondHalf = "\",\"RecordCount\":\"1\",\"RecordStartPosition\":\"0\",\"Sort\":{\"Option\":\"SortByUnitPrice\",\"Direction\":\"Ascending\",\"SortParameterId\":\"50\"},\"RequestedQuantity\":\"50\"}"
+	payload = payloadFirstHalf+model+payloadSecondHalf
+	clientID = config.HT6_CLIENT_ID
+	accessToken = config.HT6_ACCESS_TOKEN
 	headers = {
-		'x-ibm-client-id': client_id,
+		'x-ibm-client-id': clientID,
 		'x-digikey-locale-site': "CA",
 		'x-digikey-locale-language': "en",
 		'x-digikey-locale-currency': "CAD",
-		'authorization': access_token,
+		'authorization': accessToken,
 		'content-type': "application/json",
 		'accept': "application/json"
 		}
@@ -26,7 +29,9 @@ def get_things(model):
 	conn.request("POST", "/services/partsearch/v2/keywordsearch", payload, headers)
 
 	res = conn.getresponse()
-	data = res.read()
-
-	print(data.decode("utf-8"))
-	return data.decode("utf-8")
+	rawData = res.read()
+	data = rawData.decode("utf-8")
+	json.dumps(data)
+	jsonData = json.loads(data)
+	jsonData[0]
+	return 
