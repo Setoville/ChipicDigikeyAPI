8aad4b7871a97466e8c6b3e1c981e2fc46801267
andrew <andrewseto@live.com>
2018-08-25 11:20:37 -0400

    Adding check of acceptable categories

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ import json
 import config
 app = Flask(__name__)
 conn = http.client.HTTPSConnection("api.digikey.com")
+
+acceptableCategories = [2,21,39]
+
 @app.route('/')
 def hello_world():
 	return 'Insert model number as query!'
@@ -11,26 +14,36 @@ def hello_world():
 @app.route('/<model>/<datasheet>',methods=['GET'])
 def get_things(model,datasheet):
 
-	payloadFirstHalf = "{\"SearchOptions\":[\"CollapsePackingTypes\"],\"Keywords\":\""
-	payloadSecondHalf = "\",\"RecordCount\":\"1\",\"RecordStartPosition\":\"0\",\"Sort\":{\"Option\":\"SortByUnitPrice\",\"Direction\":\"Ascending\",\"SortParameterId\":\"50\"},\"RequestedQuantity\":\"50\"}"
-	payload = payloadFirstHalf+model+payloadSecondHalf
-	clientID = config.HT6_CLIENT_ID
-	accessToken = config.HT6_ACCESS_TOKEN
-	headers = {
-		'x-ibm-client-id': clientID,
-		'x-digikey-locale-site': "CA",
-		'x-digikey-locale-language': "en",
-		'x-digikey-locale-currency': "CAD",
-		'authorization': accessToken,
-		'content-type': "application/json",
-		'accept': "application/json"
-		}
+	while True:
+		payloadFirstHalf = "{\"SearchOptions\":[\"CollapsePackingTypes\"],\"Keywords\":\""
+		payloadSecondHalf = "\",\"RecordCount\":\"1\",\"RecordStartPosition\":\"0\",\"Sort\":{\"Option\":\"SortByUnitPrice\",\"Direction\":\"Ascending\",\"SortParameterId\":\"50\"},\"RequestedQuantity\":\"50\"}"
+		payload = payloadFirstHalf+model+payloadSecondHalf
+		clientID = config.HT6_CLIENT_ID
+		accessToken = config.HT6_ACCESS_TOKEN
+		headers = {
+			'x-ibm-client-id': clientID,
+			'x-digikey-locale-site': "CA",
+			'x-digikey-locale-language': "en",
+			'x-digikey-locale-currency': "CAD",
+			'authorization': accessToken,
+			'content-type': "application/json",
+			'accept': "application/json"
+			}
 
-	conn.request("POST", "/services/partsearch/v2/keywordsearch", payload, headers)
+		conn.request("POST", "/services/partsearch/v2/keywordsearch", payload, headers)
 
-	res = conn.getresponse()
-	rawData = res.read()
-	data = rawData.decode("utf-8")
+		res = conn.getresponse()
+		rawData = res.read()
+		data = rawData.decode("utf-8")
+
+		#category is an INT
+		category = json.loads(data)['Parts'][0]['Category']['Id']
+
+		#break if the category in the JSON is in the list of acceptable categories
+		if int(category) in acceptableCategories:
+			print("found")
+			break
+
 
 
 	#jsonDataAsJson is a JSON
@@ -38,14 +51,12 @@ def get_things(model,datasheet):
 
 	#jsonDataAsJson['Parts'] is a LIST of size ONE, 
 	# Z[0] makes it a DICT
+	# [''] gets the VALUE to the KEY
 	primaryDatasheet = jsonDataAsJson['Parts'][0]['PrimaryDatasheet']
-	print(jsonDataAsJson['Parts'][0])
-
 	
+	
+	#print(jsonDataAsJson['Parts'][0])
 
-	#print(len(jsonDataAsJson['Parts']))
-
-	#get datasheet !!
 
 
 
